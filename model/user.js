469d8723a6332214d28901e50e504debc6a1a14f
plaze <banikakash131@gamil.com>
2023-09-30 15:07:24 +0530

    feat(database): Add User model using mongoose

---
+++ model/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const jwt = require("jwt");
+const joi = require("joi");
+const passwordValidator = require("joi-password-complexity");
+
+//User Schema
+
+const userSchema = mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWTKEY, {
+    expiresIn: "7d",
+  });
+  return token;
+};
+
+const user = mongoose.model("user", userSchema);
+
+//Validator
+
+const validate = (data) => {
+  const schema = joi.object({
+    firstName: joi.string().required().label("First Name"),
+    lastName: joi.string().required().label("Last Name"),
+    email: joi.string().email().label("Email"),
+    password: passwordValidator().required().label("Password"),
+  });
+  return schema.validate(data);
+};
+
+module.exports = { user, validate };
