b149244711635279000ea63525eebb6847035665
plaze <banikakash131@gamil.com>
2023-09-30 16:16:30 +0530

    update: path updated

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { user, validate } = require("./../model/user");
+const { User, validate } = require("./../model/user");
 const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
@@ -10,14 +10,14 @@ router.post("/", async (req, res) => {
     if (error)
       return res.status(400).send({ message: error.details[0].message });
 
-    const user = user.findOne({ email: req.body.email });
+    const user = User.findOne({ email: req.body.email });
     if (user)
       return res.status(409).send({ message: "User email already exist" });
 
     const salt = await bcrypt.genSalt(Number(process.env.SALT));
     const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-    const result = await user.create({
+    const result = await User.create({
       ...req.body,
       password: hashPassword,
     });
